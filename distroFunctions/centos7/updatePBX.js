18405b02cb63f4527bff18240cd7e6823edc7a6b
Adam Skalicky <adam@M3800L-SV1.corp.scencdn.com>
2020-10-10 00:07:07 -0700

    updating logging to define command executed

--- distroFunctions/centos7/updatePBX.js
+++ distroFunctions/centos7/updatePBX.js
@@ -2,7 +2,10 @@ const log = require('../../lib/log');
 
 module.exports.updatePBX = async (ssh) => {
     const serverName = ssh.connection.config.host;
+    log.serverOutput('-Running: ' + 'fwconsole ma updateall', serverName)
     await ssh.execCommand('fwconsole ma updateall').then((res) => (log.serverOutput(res.stdout, serverName)))
+    log.serverOutput('-Running: ' + 'fwconsole chown', serverName)
     await ssh.execCommand('fwconsole chown').then((res) => (log.serverOutput(res.stdout, serverName)))
+    log.serverOutput('-Running: ' + 'fwconsole reload', serverName)
     await ssh.execCommand('fwconsole reload').then((res) => (log.serverOutput(res.stdout, serverName)))
 }
--- distroFunctions/ubuntu20/aptCleanup.js
+++ distroFunctions/ubuntu20/aptCleanup.js
@@ -2,5 +2,6 @@ const log = require('../../lib/log');
 
 module.exports.aptCleanup = async (ssh) => {
     const serverName = ssh.connection.config.host;
+    log.serverOutput('-Running: ' + 'apt autoremove -y', serverName)
     await ssh.execCommand('apt autoremove -y').then((res) => (log.serverOutput(res.stdout, serverName)))
 }
