6c5cbf90d87e77519369831cdf7570f25bcc0455
Adam Skalicky <adam@M3800L-SV1.corp.scencdn.com>
2020-10-10 00:06:10 -0700

    updating logging to define command executed

--- distroFunctions/centos7/updateOS.js
+++ distroFunctions/centos7/updateOS.js
@@ -2,5 +2,6 @@ const log = require('../../lib/log');
 
 module.exports.updateOS = async (ssh) => {
     const serverName = ssh.connection.config.host;
+    log.serverOutput('Running: ' + 'yum update -y', serverName)
     await ssh.execCommand('yum update -y').then((res) => (log.serverOutput(res.stdout, serverName)))
 }
--- distroFunctions/ubuntu20/updateOS.js
+++ distroFunctions/ubuntu20/updateOS.js
@@ -2,7 +2,10 @@ const log = require('../../lib/log');
 
 module.exports.updateOS = async (ssh) => {
     const serverName = ssh.connection.config.host;
+    log.serverOutput('-Running: ' + 'apt-get update', serverName)
     await ssh.execCommand('apt-get update').then((res) => (log.serverOutput(res.stdout, serverName)))
+    log.serverOutput('-Running: ' + 'apt-get upgrade -y', serverName)
     await ssh.execCommand('apt-get upgrade -y').then((res) => (log.serverOutput(res.stdout,serverName)))
+    log.serverOutput('-Running: ' + 'apt-get dist-upgrade -y', serverName)
     await ssh.execCommand('apt-get dist-upgrade -y').then((res) => (log.serverOutput(res.stdout, serverName)))
 }
