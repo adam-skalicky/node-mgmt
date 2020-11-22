430c083b0eccbb829212b4e15dac108a71a28cc6
Adam Skalicky <adam@M3800L-SV1.corp.scencdn.com>
2020-11-21 22:18:35 -0800

    .

--- distroFunctions/common/sshCopy.js
+++ distroFunctions/common/sshCopy.js
@@ -22,7 +22,7 @@ module.exports.sshCopy = async (ssh) => {
         await ssh.execCommand('mkdir -p ~/.ssh/').then((res) => (log.serverOutput(res.stdout, serverName)))
 
         log.serverOutput('-Running: ' + 'echo "' + pubKey + '"' + '> ~/.ssh/authorized_keys', serverName),
-        await ssh.execCommand('echo "' + pubKey + '"' + '>> ~/.ssh/authorized_keys').then((res) => (log.serverOutput(res.stdout, serverName)))
+        await ssh.execCommand('echo "' + pubKey + '"' + '> ~/.ssh/authorized_keys').then((res) => (log.serverOutput(res.stdout, serverName)))
 
         log.serverOutput('-Running: ' + 'chown -R root:root /root && chmod 700 /root && chmod 700 /root/.ssh && chmod 600 /root/.ssh/authorized_keys', serverName),
         await ssh.execCommand('chown root:root /root && chmod 700 /root && chmod 700 /root/.ssh && chmod 600 /root/.ssh/authorized_keys').then((res) => (log.serverOutput(res.stdout, serverName)))
