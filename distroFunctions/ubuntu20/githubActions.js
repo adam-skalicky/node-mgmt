6db94354d2aadd9a840af080c167e892eb0bdedb
Adam Skalicky <adam@M3800L-SV1.corp.scencdn.com>
2020-11-21 23:19:06 -0800

    fixed logging issue.

--- distroFunctions/ubuntu20/githubActions.js
+++ distroFunctions/ubuntu20/githubActions.js
@@ -16,20 +16,17 @@ module.exports.configureAgent = async (ssh) => {
     let bashrcPresent = bashrc.includes(allowRunnerRoot)
     if ( bashrcPresent === false){
         log.log('Adding bashrc config.', 'debug')
-        log.serverOutput('-Running: ' + 'cd ~/ && mkdir actions-runner && cd actions-runner', serverName)
-        await ssh.execCommand('echo ' + allowRunnerRoot + " >> ~/.bashrc").then((res) => (log.serverOutput(res.stdout,serverName)))
+        log.serverOutput('-Running: ' + 'echo ' + allowRunnerRoot + ' >> ~/.bashrc', serverName)
+        await ssh.execCommand('echo ' + allowRunnerRoot + ' >> ~/.bashrc').then((res) => (log.serverOutput(res.stdout,serverName)))
     }
     log.serverOutput('-Running: ' + 'cd ~/ && mkdir actions-runner && cd actions-runner', serverName)
-    await ssh.execCommand('cd ~/ && mkdir actions-runner').then((res) => (log.serverOutput(res.stdout,serverName)))
+    await ssh.execCommand('cd ~/ && mkdir -p actions-runner').then((res) => (log.serverOutput(res.stdout,serverName)))
 
     log.serverOutput('-Running: ' + 'cd ~/actions-runner && curl -O -L https://github.com/actions/runner/releases/download/v' + currentVersion + '/actions-runner-linux-x64-' + currentVersion + '.tar.gz', serverName)
     await ssh.execCommand('cd ~/actions-runner && curl -O -L https://github.com/actions/runner/releases/download/v' + currentVersion + '/actions-runner-linux-x64-' + currentVersion + '.tar.gz').then((res) => (log.serverOutput(res.stdout, serverName)))
 
     log.serverOutput('-Running: ' + 'cd ~/actions-runner && tar xzf ./actions-runner-linux-x64-' + currentVersion + '.tar.gz', serverName)
     await ssh.execCommand('cd ~/actions-runner && tar xzf ./actions-runner-linux-x64-' + currentVersion + '.tar.gz').then((res) => (log.serverOutput(res.stdout, serverName)))
-
-    log.serverOutput('-Running: ' + 'cd ~/actions-runner && ./config.sh --url ' + repoURL +  '--token ' + token, serverName)
-    await ssh.execCommand('cd ~/actions-runner && ./config.sh --url ' + repoURL +  '--token ' + token).then((res) => (log.serverOutput(res.stdout, serverName)))
 
     log.serverOutput('-Running: ' + 'cd ~/actions-runner && ./config.sh --url ' + repoURL +  '--token ' + token, serverName)
     await ssh.execCommand('cd ~/actions-runner && ./config.sh --url ' + repoURL +  '--token ' + token).then((res) => (log.serverOutput(res.stdout, serverName)))
