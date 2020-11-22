687fe310278eeeb5263dd0794d10fc5012298eb3
Adam Skalicky <adam@M3800L-SV1.corp.scencdn.com>
2020-11-21 21:40:34 -0800

    Updated github actions script with cleanup.

--- distroFunctions/ubuntu20/githubActions.js
+++ distroFunctions/ubuntu20/githubActions.js
@@ -6,13 +6,22 @@ const token = config.githubConfig.token;
 
 module.exports.configureAgent = async (ssh) => {
     const serverName = ssh.connection.config.host;
-    await ssh.execCommand('rm -r ~/actions-runner/ ~/actions-runner-linux-x64-2.274.2.tar.gz ~/bin/ ~/config.sh ~/env.sh ~/run.sh ~/externals/').then((res) => (log.serverOutput(res.stdout,serverName)))
+
     log.serverOutput('-Running: ' + 'cd ~/ && mkdir actions-runner && cd actions-runner', serverName)
     await ssh.execCommand('cd ~/ && mkdir actions-runner').then((res) => (log.serverOutput(res.stdout,serverName)))
+
     log.serverOutput('-Running: ' + 'cd ~/actions-runner && curl -O -L https://github.com/actions/runner/releases/download/v' + currentVersion + '/actions-runner-linux-x64-' + currentVersion + '.tar.gz', serverName)
     await ssh.execCommand('cd ~/actions-runner && curl -O -L https://github.com/actions/runner/releases/download/v' + currentVersion + '/actions-runner-linux-x64-' + currentVersion + '.tar.gz').then((res) => (log.serverOutput(res.stdout, serverName)))
+
     log.serverOutput('-Running: ' + 'cd ~/actions-runner && tar xzf ./actions-runner-linux-x64-' + currentVersion + '.tar.gz', serverName)
     await ssh.execCommand('cd ~/actions-runner && tar xzf ./actions-runner-linux-x64-' + currentVersion + '.tar.gz').then((res) => (log.serverOutput(res.stdout, serverName)))
+
     log.serverOutput('-Running: ' + 'cd ~/actions-runner && ./config.sh --url ' + repoURL +  '--token ' + token, serverName)
     await ssh.execCommand('cd ~/actions-runner && ./config.sh --url ' + repoURL +  '--token ' + token).then((res) => (log.serverOutput(res.stdout, serverName)))
+
+    log.serverOutput('-Running: ' + 'cd ~/actions-runner && ./config.sh --url ' + repoURL +  '--token ' + token, serverName)
+    await ssh.execCommand('cd ~/actions-runner && ./config.sh --url ' + repoURL +  '--token ' + token).then((res) => (log.serverOutput(res.stdout, serverName)))
+
+    log.serverOutput('-Running: ' + 'rm ~/actions-runner/actions-runner-linux-x64-' + currentVersion + '.tar.gz', serverName)
+    await ssh.execCommand('rm ~/actions-runner/actions-runner-linux-x64-' + currentVersion + '.tar.gz').then((res) => (log.serverOutput(res.stdout, serverName)))
 }
