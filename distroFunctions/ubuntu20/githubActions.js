825651d3d24ad9aa8772bebf80032579c668f773
Adam Skalicky <adam@M3800L-SV1.corp.scencdn.com>
2020-11-21 23:47:23 -0800

    Corrected syntac on action setup.

--- distroFunctions/ubuntu20/githubActions.js
+++ distroFunctions/ubuntu20/githubActions.js
@@ -26,7 +26,7 @@ module.exports.configureAgent = async (ssh) => {
     log.serverOutput('-Running: ' + 'cd ~/actions-runner && tar xzf ./actions-runner-linux-x64-' + currentVersion + '.tar.gz', serverName)
     await ssh.execCommand('cd ~/actions-runner && tar xzf ./actions-runner-linux-x64-' + currentVersion + '.tar.gz').then((res) => (log.serverOutput(res.stdout, serverName)))
 
-    log.serverOutput('-Running: ' + 'cd ~/actions-runner && ./config.sh --url ' + repoURL +  '--token ' + token, serverName)
+    log.serverOutput('-Running: ' + 'cd ~/actions-runner && ./config.sh --url ' + repoURL +  ' --token ' + token, serverName)
     await ssh.execCommand('cd ~/actions-runner && ' + allowRunnerRoot + '&& ./config.sh --url ' + repoURL +  '--token ' + token).then((res) => (log.serverOutput(res.stdout, serverName)))
 
     log.serverOutput('-Running: ' + 'rm ~/actions-runner/actions-runner-linux-x64-' + currentVersion + '.tar.gz', serverName)
