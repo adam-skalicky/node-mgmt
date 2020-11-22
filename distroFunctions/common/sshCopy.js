4e0a8f3d6a4c96e9ee602f6c2098b16f2e304b27
Adam Skalicky <adam@M3800L-SV1.corp.scencdn.com>
2020-11-21 22:26:00 -0800

    Reverted

--- distroFunctions/common/sshCopy.js
+++ distroFunctions/common/sshCopy.js
@@ -13,7 +13,7 @@ module.exports.sshCopy = async (ssh) => {
         existingAuthorizedKeys = res.stdout
         ))
     let authorizedKeyPresent = existingAuthorizedKeys.includes(pubKey)
-    if ( authorizedKeyPresent === true){
+    if ( authorizedKeyPresent === false){
         log.log('Adding authorized key.', 'debug')
 
 
@@ -21,8 +21,8 @@ module.exports.sshCopy = async (ssh) => {
         log.serverOutput('-Running: ' + 'mkdir -p ~/.ssh/', serverName),
         await ssh.execCommand('mkdir -p ~/.ssh/').then((res) => (log.serverOutput(res.stdout, serverName)))
 
-        log.serverOutput('-Running: ' + 'echo "' + pubKey + '"' + '> ~/.ssh/authorized_keys', serverName),
-        await ssh.execCommand('echo "' + pubKey + '"' + '> ~/.ssh/authorized_keys').then((res) => (log.serverOutput(res.stdout, serverName)))
+        log.serverOutput('-Running: ' + 'echo "' + pubKey + '"' + '>> ~/.ssh/authorized_keys', serverName),
+        await ssh.execCommand('echo "' + pubKey + '"' + '>> ~/.ssh/authorized_keys').then((res) => (log.serverOutput(res.stdout, serverName)))
 
         log.serverOutput('-Running: ' + 'chown -R root:root /root && chmod 700 /root && chmod 700 /root/.ssh && chmod 600 /root/.ssh/authorized_keys', serverName),
         await ssh.execCommand('chown root:root /root && chmod 700 /root && chmod 700 /root/.ssh && chmod 600 /root/.ssh/authorized_keys').then((res) => (log.serverOutput(res.stdout, serverName)))
