7cd778fff7c605f135a38b37be34426e276502b9
Adam Skalicky <adam.skalicky@scencdn.com>
2020-12-10 06:33:05 +0000

    Added snap refresh to ubuntu update

--- distroFunctions/ubuntu20/updateOS.js
+++ distroFunctions/ubuntu20/updateOS.js
@@ -8,4 +8,6 @@ module.exports.updateOS = async (ssh) => {
     await ssh.execCommand('apt-get upgrade -y').then((res) => (log.serverOutput(res.stdout,serverName)))
     log.serverOutput('-Running: ' + 'apt-get dist-upgrade -y', serverName)
     await ssh.execCommand('apt-get dist-upgrade -y').then((res) => (log.serverOutput(res.stdout, serverName)))
+    log.serverOutput('-Running: ' + 'snap refresh ', serverName)
+    await ssh.execCommand('snap refresh').then((res) => (log.serverOutput(res.stdout, serverName)))
 }
